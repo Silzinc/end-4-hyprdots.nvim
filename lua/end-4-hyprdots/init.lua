0dbf2d0c4e6e1003ca73ee85730ee18e29c63877
Silzinc <jonasbloch854@proton.me>
2025-07-19 20:06:02 +0200

    feat: generate light and dark schemes on startup.
    Dark and light colors are generated using the
    generate_colors_material.py script from end-4's hyprland dotfiles. The
    generation takes half a second (pretty long), so it is avoided when the
    (cached) path to wallpaper and the illogical-impulse scheme have not
    changed.

--- lua/end-4-hyprdots/init.lua
+++ lua/end-4-hyprdots/init.lua
@@ -187,7 +187,7 @@ function M.setup(user_conf)
 	M.options.highlight_overrides.all = user_conf.custom_highlights or M.options.highlight_overrides.all
 	-- Load generated colors
 	local end_4_hyprdots_palette = require "end-4-hyprdots.lib.generate_colors" or {}
-	M.options.color_overrides = vim.tbl_deep_extend("keep", M.options.color_overrides, { all = end_4_hyprdots_palette })
+	M.options.color_overrides = vim.tbl_deep_extend("keep", M.options.color_overrides, end_4_hyprdots_palette)
 
 	-- Get cached hash
 	local cached_path = M.options.compile_path .. M.path_sep .. "cached"
@@ -201,7 +201,7 @@ function M.setup(user_conf)
 	-- Get current hash
 	local git_path = debug.getinfo(1).source:sub(2, -24) .. ".git"
 	local git = vim.fn.getftime(git_path) -- 2x faster vim.loop.fs_stat
-	local hash = require("end-4-hyprdots.lib.hashing").hash { user_conf, end_4_hyprdots_palette }
+	local hash = require("end-4-hyprdots.lib.hashing").hash(user_conf)
 		.. (git == -1 and git_path or git) -- no .git in /nix/store -> cache path
 		.. (vim.o.winblend == 0 and 1 or 0) -- :h winblend
 		.. (vim.o.pumblend == 0 and 1 or 0) -- :h pumblend
--- lua/end-4-hyprdots/lib/generate_colors.lua
+++ lua/end-4-hyprdots/lib/generate_colors.lua
@@ -1,5 +1,6 @@
 local function report_error(msg) vim.notify("end-4-hyprdots theme cannot load: " .. msg, vim.diagnostic.severity.ERROR) end
 local blend = require("end-4-hyprdots.utils.colors").blend
+local decode = require("end-4-hyprdots.utils.json").decode
 
 local join = vim.fs.joinpath
 local isdir = vim.fn.isdirectory
@@ -8,64 +9,126 @@ local isfile = vim.fn.filereadable
 local home = vim.env.HOME or os.getenv "HOME"
 
 local xdg_state_home = os.getenv "XDG_STATE_HOME" or join(home, ".local", "state")
+local xdg_config_home = os.getenv "XDG_CONFIG_HOME" or join(home, ".config")
 
-if not isdir(xdg_state_home) then
-	report_error(string.format("State directory '%s' does not exist.", xdg_state_home))
+local config_path = join(xdg_config_home, "illogical-impulse", "config.json")
+local config_file = io.open(config_path)
+if not config_file then
+	report_error(string.format("illogical-impulse config file '%s' does not exist.", config_path))
 	return
 end
 
-local colors_path = join(xdg_state_home, "quickshell", "user", "generated", "material_colors.scss")
-if not isfile(colors_path) then
-	report_error(string.format("Colors file '%s' does not exist.", colors_path))
-	return
+local config = decode(config_file:read "*a")
+config_file:close()
+
+local scheme = config.appearance.palette.type
+local bg_path = config.background.wallpaperPath
+local termscheme = join(xdg_config_home, "quickshell", "ii", "scripts", "colors", "terminal", "scheme-base.json")
+local colorgen_cache = join(xdg_state_home, "quickshell", "user", "generated", "color.txt")
+local colorgen_script = join(xdg_config_home, "quickshell", "ii", "scripts", "colors", "generate_colors_material.py")
+
+-- It takes about half a second to load the material colors from the python script.
+-- Since it only depends on the background image and the illogical-impulse scheme,
+-- the results are not queried if these settings did not change.
+-- The actual colors are stored by catppuccin's compilation routine. Thus, return
+-- an empty object, these will be loaded as wanted.
+local cache_path = join(require("end-4-hyprdots").options.compile_path, "illogical-impulse-params")
+local f = loadfile(cache_path)
+
+local key_str = table.concat({ scheme, bg_path }, "\0")
+
+-- Check if the stored and current values are equal and return {} is so.
+if f and f() == key_str then return {} end
+
+-- Else, save the params for the next load. string.dump hardcodes its content as a string,
+-- without compiling it (key_str would be stored as 'key_str'). loadstring bypasses this
+-- by evaluating its input as lua code.
+local bin = string.dump(assert(loadstring(string.format('return "%s"', key_str))))
+
+local cache_file = assert(
+	io.open(cache_path, "wb"),
+	"end-4-hyprdots theme cannot load: failed to open illogical-impulse params cache file '" .. cache_path .. "'."
+)
+cache_file:write(bin)
+cache_file:close()
+
+---@param mode "light" | "dark"
+---@return table
+local function get_palette(mode)
+	local src = vim.system({
+		"python3",
+		colorgen_script,
+		"--path",
+		bg_path,
+		"--mode",
+		mode,
+		"--scheme",
+		scheme,
+		"--termscheme",
+		termscheme,
+		"--blend_bg_fg",
+		"--cache",
+		colorgen_cache,
+	}, { text = true }):wait()
+
+	local colors = {}
+
+	if src.code ~= 0 then
+		report_error(string.format(
+			[[failed to generate the palette of flavour '%s'.
+STDERR: %s]],
+			mode,
+			src.stderr
+		))
+		return {}
+	end
+
+	for color in src.stdout:gmatch "[^\n]+" do
+		local name, hex = color:match "%$([%w_%-]+):%s*([^%s]+);$"
+		colors[name] = hex
+	end
+
+	local dark_mode = mode == "dark"
+
+	local steps = 5 -- number of colors = steps + 1
+	local gradient = {}
+
+	local bright_surface = colors["outline"]
+	local dark_surface = colors["surfaceContainerHighest"]
+
+	for step = 0, steps do
+		local alpha = 1.0 - step / steps
+		gradient[step + 1] = blend(bright_surface, dark_surface, alpha)
+	end
+
+	return {
+		flamingo = dark_mode and "#f0c6c6" or "#dd7878",
+		rosewater = dark_mode and "#f4dbd6" or "#dc8a78",
+		pink = colors["term5"],
+		mauve = colors["term13"],
+		red = colors["term1"],
+		maroon = colors["term9"],
+		peach = colors["term3"],
+		yellow = colors["term11"],
+		green = colors["term2"],
+		teal = colors["term10"],
+		sky = colors["term6"],
+		sapphire = colors["term14"],
+		blue = colors["term4"],
+		lavender = colors["term12"],
+		text = colors["term7"],
+		subtext1 = colors["onSurface"],
+		subtext0 = colors["onSurfaceVariant"],
+		overlay2 = gradient[1],
+		overlay1 = gradient[2],
+		overlay0 = gradient[3],
+		surface2 = gradient[4],
+		surface1 = gradient[5],
+		surface0 = gradient[6],
+		base = colors["term0"],
+		mantle = colors["surfaceContainerLow"],
+		crust = colors["surfaceContainerLowest"],
+	}
 end
 
-local colors = {}
-
-for color in io.lines(colors_path) do
-	color = tostring(color)
-	local name, hex = color:match "%$([%w_%-]+):%s*([^%s]+);$"
-	colors[name] = hex
-end
-
-local dark_mode = colors.darkmode == "True"
-
-local steps = 5 -- number of colors = steps + 1
-local gradient = {}
-
-local bright_surface = colors["outline"]
-local dark_surface = colors["surfaceContainerHighest"]
-
-for step = 0, steps do
-	local alpha = 1.0 - step / steps
-	gradient[step + 1] = blend(bright_surface, dark_surface, alpha)
-end
-
-return {
-	flamingo = dark_mode and "#f0c6c6" or "#dd7878",
-	rosewater = dark_mode and "#f4dbd6" or "#dc8a78",
-	pink = colors["term5"],
-	mauve = colors["term13"],
-	red = colors["term1"],
-	maroon = colors["term9"],
-	peach = colors["term3"],
-	yellow = colors["term11"],
-	green = colors["term2"],
-	teal = colors["term10"],
-	sky = colors["term6"],
-	sapphire = colors["term14"],
-	blue = colors["term4"],
-	lavender = colors["term12"],
-	text = colors["term7"],
-	subtext1 = colors["onSurface"],
-	subtext0 = colors["onSurfaceVariant"],
-	overlay2 = gradient[1],
-	overlay1 = gradient[2],
-	overlay0 = gradient[3],
-	surface2 = gradient[4],
-	surface1 = gradient[5],
-	surface0 = gradient[6],
-	base = colors["term0"],
-	mantle = colors["surfaceContainerLow"],
-	crust = colors["surfaceContainerLowest"],
-}
+return { dark = get_palette "dark", light = get_palette "light" }
